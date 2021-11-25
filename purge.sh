d3245f1a32e5d722445f1e690f1be899c7aa27a2
Christian Zufferey <christian@zufferey.com>
2021-11-25 20:25:30 +0100

    fix: amélioré le script de purge, car il restait les caches du build de l'image

--- purge.sh
+++ purge.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #Petit script pour nettoyer tout le binz du docker-compose
-#zf211125.0905
+#zf211125.2022
+
+read -p "Etes-vous certain de vouloir tout effacer ce qui ne tourne pas au niveau Docker sur votre machine, donc un system prune ?"
+read -p "Mais cela va VRAIMENT VRAIMENT tout effacer ce qui ne tourne pas, donc un prune ?"
+
+docker system prune -a -f --volumes
+
+exit
 
 docker-compose down -v --remove-orphans
 docker image rm zalpine:latest
